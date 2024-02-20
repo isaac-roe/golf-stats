50c795a00bf6c01466ad96a6c722b69d8b97e3ba
isaacr34 <isaac.roe@my.simpson.edu>
2024-02-20 12:45:02 -0600

    Updates made in class

--- src/main/Main.java
+++ src/main/Main.java
@@ -71,15 +71,112 @@ public class Main extends Application {
             System.out.println(ans);
         });
 
+        // Scene 2 textfield and label setup
+        Label scoreLab = new Label("Enter score for the hole");
+        Label distLab = new Label("Enter distance for the hole");
+        Label hole1 = new Label("Hole 1");
+        Label hole2 = new Label("Hole 2");
+        Label hole3 = new Label("Hole 3");
+        Label hole4 = new Label("Hole 4");
+        Label hole5 = new Label("Hole 5");
+        Label hole6 = new Label("Hole 6");
+        Label hole7 = new Label("Hole 7");
+        Label hole8 = new Label("Hole 8");
+        Label hole9 = new Label("Hole 9");
+
+        TextField h1Dist = new TextField();
+        h1Dist.setMaxWidth(60);
+        TextField h2Dist = new TextField();
+        h2Dist.setMaxWidth(60);
+        TextField h3Dist = new TextField();
+        h3Dist.setMaxWidth(60);
+        TextField h4Dist = new TextField();
+        h4Dist.setMaxWidth(60);
+        TextField h5Dist = new TextField();
+        h5Dist.setMaxWidth(60);
+        TextField h6Dist = new TextField();
+        h6Dist.setMaxWidth(60);
+        TextField h7Dist = new TextField();
+        h7Dist.setMaxWidth(60);
+        TextField h8Dist = new TextField();
+        h8Dist.setMaxWidth(60);
+        TextField h9Dist = new TextField();
+        h9Dist.setMaxWidth(60);
+
+        TextField h1Score = new TextField();
+        h1Score.setMaxWidth(60);
+        TextField h2Score = new TextField();
+        h2Score.setMaxWidth(60);
+        TextField h3Score = new TextField();
+        h3Score.setMaxWidth(60);
+        TextField h4Score = new TextField();
+        h4Score.setMaxWidth(60);
+        TextField h5Score = new TextField();
+        h5Score.setMaxWidth(60);
+        TextField h6Score = new TextField();
+        h6Score.setMaxWidth(60);
+        TextField h7Score = new TextField();
+        h7Score.setMaxWidth(60);
+        TextField h8Score = new TextField();
+        h8Score.setMaxWidth(60);
+        TextField h9Score = new TextField();
+        h9Score.setMaxWidth(60);
 
         //Scene 2
+        GridPane gridTwo = new GridPane();
+        gridTwo.setAlignment(Pos.TOP_CENTER);
+        gridTwo.setHgap(10);
+        gridTwo.setVgap(10);
+        gridTwo.setPadding(new Insets(25, 25, 25, 25));
+
         Label label2= new Label("This is the second scene");
         Button button2= new Button("Go to scene 1");
         button2.setOnAction(e -> primaryStage.setScene(scene));
-        VBox layout2= new VBox(20);
-        layout2.getChildren().addAll(label2, button2);
-        Scene sceneTwo = new Scene(layout2,750,500);
 
+        gridTwo.add(label2, 1, 0);
+        gridTwo.add(button2, 2, 0);
+        gridTwo.add(distLab, 1, 1);
+        gridTwo.add(scoreLab, 2, 1);
+
+        gridTwo.add(hole1, 0, 2);
+        gridTwo.add(h1Dist, 1, 2);
+        gridTwo.add(h1Score, 2, 2);
+
+        gridTwo.add(hole2, 0, 3);
+        gridTwo.add(h2Dist, 1, 3);
+        gridTwo.add(h2Score, 2, 3);
+
+        gridTwo.add(hole3, 0, 4);
+        gridTwo.add(h3Dist, 1, 4);
+        gridTwo.add(h3Score, 2, 4);
+
+        gridTwo.add(hole4, 0, 5);
+        gridTwo.add(h4Dist, 1, 5);
+        gridTwo.add(h4Score, 2, 5);
+
+        gridTwo.add(hole5, 0, 6);
+        gridTwo.add(h5Dist, 1, 6);
+        gridTwo.add(h5Score, 2, 6);
+
+        gridTwo.add(hole6, 0, 7);
+        gridTwo.add(h6Dist, 1, 7);
+        gridTwo.add(h6Score, 2, 7);
+
+        gridTwo.add(hole7, 0, 8);
+        gridTwo.add(h7Dist, 1, 8);
+        gridTwo.add(h7Score, 2, 8);
+
+        gridTwo.add(hole8, 0, 9);
+        gridTwo.add(h8Dist, 1, 9);
+        gridTwo.add(h8Score, 2, 9);
+
+        gridTwo.add(hole9, 0, 10);
+        gridTwo.add(h9Dist, 1, 10);
+        gridTwo.add(h9Score, 2, 10);
+
+//        VBox layout2= new VBox(20);
+//        layout2.getChildren().addAll(label2, button2, h1Dist, h1Score, h2Dist, h2Score);
+        Scene sceneTwo = new Scene(gridTwo,750,500);
 
         Button btnTwo = new Button("Test");
         HBox hbBtnTwo = new HBox(10);
--- src/main/UtilityFunctions.java
+++ src/main/UtilityFunctions.java
@@ -1,6 +1,44 @@
 package main;
 
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
 public class UtilityFunctions {
+    // Set up for a hash map to store expected strokes to hole out for
+    // a position of off the tee.
+    private static final NavigableMap<Integer, Double> sgTee = new TreeMap<>();
+    static {
+        sgTee.put(10, 2.20);
+        sgTee.put(15, 2.30);
+        sgTee.put(20, 2.40);
+        sgTee.put(25, 2.45);
+        sgTee.put(30, 2.50);
+        sgTee.put(35, 2.55);
+        sgTee.put(40, 2.60);
+        sgTee.put(45, 2.62);
+        sgTee.put(50, 2.65);
+        sgTee.put(55, 2.67);
+        sgTee.put(60, 2.70);
+        sgTee.put(70, 2.73);
+        sgTee.put(75, 2.74);
+        sgTee.put(80, 2.75);
+        sgTee.put(85, 2.77);
+        sgTee.put(90, 2.78);
+        sgTee.put(95, 2.79);
+        sgTee.put(100, 2.80);
+        sgTee.put(105, 2.81);
+        sgTee.put(110, 2.83);
+        sgTee.put(115, 2.84);
+        sgTee.put(120, 2.85);
+        sgTee.put(125, 2.86);
+        sgTee.put(130, 2.88);
+        sgTee.put(135, 2.90);
+        sgTee.put(140, 2.91);
+        sgTee.put(145, 2.93);
+        sgTee.put(150, 2.95);
+        sgTee.put(155, 2.97);
+    }
+
     public static double calculateHole(int holeLength, int strokes)
     {
         double gained = 0;
@@ -14,9 +52,24 @@ public class UtilityFunctions {
     {
         double expectedStrokes = 0;
 
+
+
         if (holeLength <= 10) expectedStrokes = 2.20;
         else if (holeLength >= 60) expectedStrokes = 2.70;
 
         return expectedStrokes;
+    }
+
+    public static Long nearestKey(TreeMap<Long, Object> map, Long target) {
+        double minDiff = Double.MAX_VALUE;
+        Long nearest = null;
+        for (long key : map.keySet()) {
+            double diff = Math.abs((double) target - (double) key);
+            if (diff < minDiff) {
+                nearest = key;
+                minDiff = diff;
+            }
+        }
+        return nearest;
     }
 }
