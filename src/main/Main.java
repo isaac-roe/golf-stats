1da14e6f0c26fe29c701b7bb0b0e3f0b0f4e3c0c
isaacr34 <isaac.roe@my.simpson.edu>
2024-04-09 12:48:07 -0500

    Changed a lot

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -41,6 +42,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
 
         TextField test = new TextField();
+        TextField test2 = new TextField();
 
         Label l = new Label("Enter score");
 
@@ -56,24 +58,25 @@ public class Main extends Application {
         test.setOnAction(testEvent);
 
         grid.add(test, 5, 5);
+        grid.add(test2, 7, 7);
         grid.add(l, 5, 6);
 
         Button btn = new Button("Test");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
+//        hbBtn.getChildren().add(btn);
+//        grid.add(hbBtn, 1, 4);
 
         primaryStage.show();
 
         btn.setOnAction((calculate) -> {
-            double ans = UtilityFunctions.calculateHole(70, 2);
-            System.out.println(ans);
+//            double ans = UtilityFunctions.calculateHole(Integer.parseInt(test.getText()), Integer.parseInt(test2.getText()), );
+//            System.out.println(ans);
         });
 
         // Scene 2 textfield and label setup
         Label scoreLab = new Label("Enter score for the hole");
-        Label distLab = new Label("Enter distance for the hole");
+        Label distLab = new Label("Enter distance to the hole");
         Label hole1 = new Label("Hole 1");
         Label hole2 = new Label("Hole 2");
         Label hole3 = new Label("Hole 3");
@@ -83,6 +86,15 @@ public class Main extends Application {
         Label hole7 = new Label("Hole 7");
         Label hole8 = new Label("Hole 8");
         Label hole9 = new Label("Hole 9");
+        Label hole10 = new Label("Hole 10");
+        Label hole11 = new Label("Hole 11");
+        Label hole12 = new Label("Hole 12");
+        Label hole13 = new Label("Hole 13");
+        Label hole14 = new Label("Hole 14");
+        Label hole15 = new Label("Hole 15");
+        Label hole16 = new Label("Hole 16");
+        Label hole17 = new Label("Hole 17");
+        Label hole18 = new Label("Hole 18");
 
         TextField h1Dist = new TextField();
         h1Dist.setMaxWidth(60);
@@ -102,6 +114,24 @@ public class Main extends Application {
         h8Dist.setMaxWidth(60);
         TextField h9Dist = new TextField();
         h9Dist.setMaxWidth(60);
+        TextField h10Dist = new TextField();
+        h10Dist.setMaxWidth(60);
+        TextField h11Dist = new TextField();
+        h11Dist.setMaxWidth(60);
+        TextField h12Dist = new TextField();
+        h12Dist.setMaxWidth(60);
+        TextField h13Dist = new TextField();
+        h13Dist.setMaxWidth(60);
+        TextField h14Dist = new TextField();
+        h14Dist.setMaxWidth(60);
+        TextField h15Dist = new TextField();
+        h15Dist.setMaxWidth(60);
+        TextField h16Dist = new TextField();
+        h16Dist.setMaxWidth(60);
+        TextField h17Dist = new TextField();
+        h17Dist.setMaxWidth(60);
+        TextField h18Dist = new TextField();
+        h18Dist.setMaxWidth(60);
 
         TextField h1Score = new TextField();
         h1Score.setMaxWidth(60);
@@ -121,6 +151,47 @@ public class Main extends Application {
         h8Score.setMaxWidth(60);
         TextField h9Score = new TextField();
         h9Score.setMaxWidth(60);
+        TextField h10Score = new TextField();
+        h10Score.setMaxWidth(60);
+        TextField h11Score = new TextField();
+        h11Score.setMaxWidth(60);
+        TextField h12Score = new TextField();
+        h12Score.setMaxWidth(60);
+        TextField h13Score = new TextField();
+        h13Score.setMaxWidth(60);
+        TextField h14Score = new TextField();
+        h14Score.setMaxWidth(60);
+        TextField h15Score = new TextField();
+        h15Score.setMaxWidth(60);
+        TextField h16Score = new TextField();
+        h16Score.setMaxWidth(60);
+        TextField h17Score = new TextField();
+        h17Score.setMaxWidth(60);
+        TextField h18Score = new TextField();
+        h18Score.setMaxWidth(60);
+
+        TextField h1s1 = new TextField(); h1s1.setMaxWidth(50);
+        ChoiceBox h1s1loc = new ChoiceBox();
+        h1s1loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        ChoiceBox h1s2loc = new ChoiceBox();
+        h1s2loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        TextField h1s2 = new TextField(); h1s2.setMaxWidth(50);
+        ChoiceBox h1s3loc = new ChoiceBox();
+        h1s3loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        TextField h1s3 = new TextField(); h1s3.setMaxWidth(50);
+        ChoiceBox h1s4loc = new ChoiceBox();
+        h1s4loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        TextField h1s4 = new TextField(); h1s4.setMaxWidth(50);
+        ChoiceBox h1s5loc = new ChoiceBox();
+        h1s5loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        TextField h1s5 = new TextField(); h1s5.setMaxWidth(50);
+        ChoiceBox h1s6loc = new ChoiceBox();
+        h1s6loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        TextField h1s6 = new TextField(); h1s6.setMaxWidth(50);
+        ChoiceBox h1s7loc = new ChoiceBox();
+        h1s7loc.getItems().addAll("Tee", "Fairway", "Rough", "Green");
+        TextField h1s7 = new TextField(); h1s7.setMaxWidth(50);
+
 
         //Scene 2
         GridPane gridTwo = new GridPane();
@@ -130,53 +201,164 @@ public class Main extends Application {
         gridTwo.setPadding(new Insets(25, 25, 25, 25));
 
         Label label2= new Label("This is the second scene");
-        Button button2= new Button("Go to scene 1");
+        Button button2= new Button("Go to main screen");
         button2.setOnAction(e -> primaryStage.setScene(scene));
+
+        Button button3 = new Button("Indianola");
+        button3.setOnAction((event) -> {
+            h1Dist.setText("607"); h2Dist.setText("380"); h3Dist.setText("560");
+            h4Dist.setText("187"); h5Dist.setText("560"); h6Dist.setText("360");
+            h7Dist.setText("175"); h8Dist.setText("341"); h9Dist.setText("160");
+            h10Dist.setText("348"); h11Dist.setText("340"); h12Dist.setText("363");
+            h13Dist.setText("564"); h14Dist.setText("356"); h15Dist.setText("348");
+            h16Dist.setText("120"); h17Dist.setText("330"); h18Dist.setText("205");
+        });
+
+        Label result = new Label("Strokes Gained: ");
+
+        Button buttonCalculate9 = new Button("Calculate strokes gained.");
+        buttonCalculate9.setOnAction((event) -> {
+
+//            for (int i = Integer.parseInt(h1Score.getText()); i > 0; i--) {
+//
+//            }
+            double sg1 = UtilityFunctions.calculateHole(Integer.parseInt(h1Dist.getText()), Integer.parseInt(h1s1.getText()),
+                    "Tee", (String) h1s1loc.getValue());
+            double sg2 = UtilityFunctions.calculateHole(Integer.parseInt(h1s1.getText()), Integer.parseInt(h1s2.getText()),
+                    (String) h1s1loc.getValue(), (String) h1s2loc.getValue());
+            double sg3 = UtilityFunctions.calculateHole(Integer.parseInt(h1s2.getText()), Integer.parseInt(h1s3.getText()),
+                    (String) h1s2loc.getValue(), (String) h1s3loc.getValue());
+            double sg4 = UtilityFunctions.calculateHole(Integer.parseInt(h1s3.getText()), Integer.parseInt(h1s4.getText()),
+                    (String) h1s3loc.getValue(), (String) h1s4loc.getValue());
+            double sg5 = UtilityFunctions.calculateHole(Integer.parseInt(h1s4.getText()), Integer.parseInt(h1s5.getText()),
+                    (String) h1s4loc.getValue(), (String) h1s5loc.getValue());
+//            double sg1 = UtilityFunctions.calculateHole(Integer.parseInt(h1Dist.getText()), Integer.parseInt(h1Score.getText()));
+//            double sg2 = UtilityFunctions.calculateHole(Integer.parseInt(h2Dist.getText()), Integer.parseInt(h2Score.getText()));
+//            double sg3 = UtilityFunctions.calculateHole(Integer.parseInt(h3Dist.getText()), Integer.parseInt(h3Score.getText()));
+//            double sg4 = UtilityFunctions.calculateHole(Integer.parseInt(h4Dist.getText()), Integer.parseInt(h4Score.getText()));
+//            double sg5 = UtilityFunctions.calculateHole(Integer.parseInt(h5Dist.getText()), Integer.parseInt(h5Score.getText()));
+//            double sg6 = UtilityFunctions.calculateHole(Integer.parseInt(h6Dist.getText()), Integer.parseInt(h6Score.getText()));
+//            double sg7 = UtilityFunctions.calculateHole(Integer.parseInt(h7Dist.getText()), Integer.parseInt(h7Score.getText()));
+//            double sg8 = UtilityFunctions.calculateHole(Integer.parseInt(h8Dist.getText()), Integer.parseInt(h8Score.getText()));
+//            double sg9 = UtilityFunctions.calculateHole(Integer.parseInt(h9Dist.getText()), Integer.parseInt(h9Score.getText()));
+            System.out.println(sg1);
+
+////            System.out.printf("%.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f",
+////                    sg1, sg2, sg3, sg4, sg5, sg6, sg7, sg8, sg9);
+//            double total = sg1 + sg2 + sg3 + sg4 + sg5 + sg6 + sg7 + sg8 + sg9;
+            result.setText("Strokes Gained: " + String.format("%.2f, %.2f, %.2f, %.2f, %.2f", sg1, sg2, sg3, sg4, sg5));
+            });
 
         gridTwo.add(label2, 1, 0);
         gridTwo.add(button2, 2, 0);
         gridTwo.add(distLab, 1, 1);
         gridTwo.add(scoreLab, 2, 1);
+        gridTwo.add(button3, 3, 0);
 
         gridTwo.add(hole1, 0, 2);
         gridTwo.add(h1Dist, 1, 2);
         gridTwo.add(h1Score, 2, 2);
 
-        gridTwo.add(hole2, 0, 3);
-        gridTwo.add(h2Dist, 1, 3);
-        gridTwo.add(h2Score, 2, 3);
+        gridTwo.add(h1s1, 1, 3);
+        gridTwo.add(h1s1loc, 2, 3);
+        gridTwo.add(h1s2, 1, 4);
+        gridTwo.add(h1s2loc, 2, 4);
+        gridTwo.add(h1s3, 1, 5);
+        gridTwo.add(h1s3loc, 2, 5);
+        gridTwo.add(h1s4, 1, 6);
+        gridTwo.add(h1s4loc, 2, 6);
+        gridTwo.add(h1s5, 1, 7);
+        gridTwo.add(h1s5loc, 2, 7);
+        gridTwo.add(h1s6, 1, 8);
+        gridTwo.add(h1s6loc, 2, 8);
+        gridTwo.add(h1s7, 1, 9);
+        gridTwo.add(h1s7loc, 2, 9);
 
-        gridTwo.add(hole3, 0, 4);
-        gridTwo.add(h3Dist, 1, 4);
-        gridTwo.add(h3Score, 2, 4);
+//        gridTwo.add(hole2, 0, 3);
+//        gridTwo.add(h2Dist, 1, 3);
+//        gridTwo.add(h2Score, 2, 3);
+//
+//        gridTwo.add(hole3, 0, 4);
+//        gridTwo.add(h3Dist, 1, 4);
+//        gridTwo.add(h3Score, 2, 4);
+//
+//        gridTwo.add(hole4, 0, 5);
+//        gridTwo.add(h4Dist, 1, 5);
+//        gridTwo.add(h4Score, 2, 5);
+//
+//        gridTwo.add(hole5, 0, 6);
+//        gridTwo.add(h5Dist, 1, 6);
+//        gridTwo.add(h5Score, 2, 6);
+//
+//        gridTwo.add(hole6, 0, 7);
+//        gridTwo.add(h6Dist, 1, 7);
+//        gridTwo.add(h6Score, 2, 7);
+//
+//        gridTwo.add(hole7, 0, 8);
+//        gridTwo.add(h7Dist, 1, 8);
+//        gridTwo.add(h7Score, 2, 8);
+//
+//        gridTwo.add(hole8, 0, 9);
+//        gridTwo.add(h8Dist, 1, 9);
+//        gridTwo.add(h8Score, 2, 9);
+//
+//        gridTwo.add(hole9, 0, 10);
+//        gridTwo.add(h9Dist, 1, 10);
+//        gridTwo.add(h9Score, 2, 10);
+//
+//        gridTwo.add(hole10, 0, 12);
+//        gridTwo.add(h10Dist, 1, 12);
+//        gridTwo.add(h10Score, 2, 12);
+//
+//        gridTwo.add(hole11, 0, 13);
+//        gridTwo.add(h11Dist, 1, 13);
+//        gridTwo.add(h11Score, 2, 13);
 
-        gridTwo.add(hole4, 0, 5);
-        gridTwo.add(h4Dist, 1, 5);
-        gridTwo.add(h4Score, 2, 5);
+//        gridTwo.add(hole12, 0, 14);
+//        gridTwo.add(h12Dist, 1, 14);
+//        gridTwo.add(h12Score, 2, 14);
+//
+//        gridTwo.add(hole13, 0, 15);
+//        gridTwo.add(h13Dist, 1, 15);
+//        gridTwo.add(h13Score, 2, 15);
+//
+//        gridTwo.add(hole14, 0, 16);
+//        gridTwo.add(h14Dist, 1, 16);
+//        gridTwo.add(h14Score, 2, 16);
+//
+//        gridTwo.add(hole15, 0, 17);
+//        gridTwo.add(h15Dist, 1, 17);
+//        gridTwo.add(h15Score, 2, 17);
+//
+//        gridTwo.add(hole16, 0, 18);
+//        gridTwo.add(h16Dist, 1, 18);
+//        gridTwo.add(h16Score, 2, 18);
+//
+//        gridTwo.add(hole17, 0, 19);
+//        gridTwo.add(h17Dist, 1, 19);
+//        gridTwo.add(h17Score, 2, 19);
+//
+//        gridTwo.add(hole18, 0, 20);
+//        gridTwo.add(h18Dist, 1, 20);
+//        gridTwo.add(h18Score, 2, 20);
 
-        gridTwo.add(hole5, 0, 6);
-        gridTwo.add(h5Dist, 1, 6);
-        gridTwo.add(h5Score, 2, 6);
+        gridTwo.add(buttonCalculate9, 1, 11);
+        gridTwo.add(result, 2, 11);
 
-        gridTwo.add(hole6, 0, 7);
-        gridTwo.add(h6Dist, 1, 7);
-        gridTwo.add(h6Score, 2, 7);
+//        ScrollPane sp = new ScrollPane(gridTwo);
+//        gridTwo.add(sp, 0, 1);
+//        GridPane.setHgrow(sp, Priority.ALWAYS);
 
-        gridTwo.add(hole7, 0, 8);
-        gridTwo.add(h7Dist, 1, 8);
-        gridTwo.add(h7Score, 2, 8);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(gridTwo);
 
-        gridTwo.add(hole8, 0, 9);
-        gridTwo.add(h8Dist, 1, 9);
-        gridTwo.add(h8Score, 2, 9);
+//        root.getChildren().addAll(scrollPane);
+//        scene.setRoot(root);
 
-        gridTwo.add(hole9, 0, 10);
-        gridTwo.add(h9Dist, 1, 10);
-        gridTwo.add(h9Score, 2, 10);
 
 //        VBox layout2= new VBox(20);
 //        layout2.getChildren().addAll(label2, button2, h1Dist, h1Score, h2Dist, h2Score);
-        Scene sceneTwo = new Scene(gridTwo,750,500);
+        Scene sceneTwo = new Scene(scrollPane,750,500);
 
         Button btnTwo = new Button("Test");
         HBox hbBtnTwo = new HBox(10);
--- src/main/UtilityFunctions.java
+++ src/main/UtilityFunctions.java
@@ -6,44 +6,185 @@ import java.util.TreeMap;
 public class UtilityFunctions {
     // Set up for a hash map to store expected strokes to hole out for
     // a position of off the tee.
-    private static final NavigableMap<Integer, Double> sgTee = new TreeMap<>();
+    private static final TreeMap<Integer, Double> sgTee = new TreeMap<>();
     static {
-        sgTee.put(10, 2.20);
-        sgTee.put(15, 2.30);
-        sgTee.put(20, 2.40);
-        sgTee.put(25, 2.45);
-        sgTee.put(30, 2.50);
-        sgTee.put(35, 2.55);
-        sgTee.put(40, 2.60);
-        sgTee.put(45, 2.62);
-        sgTee.put(50, 2.65);
-        sgTee.put(55, 2.67);
-        sgTee.put(60, 2.70);
-        sgTee.put(70, 2.73);
-        sgTee.put(75, 2.74);
-        sgTee.put(80, 2.75);
-        sgTee.put(85, 2.77);
-        sgTee.put(90, 2.78);
-        sgTee.put(95, 2.79);
-        sgTee.put(100, 2.80);
-        sgTee.put(105, 2.81);
-        sgTee.put(110, 2.83);
-        sgTee.put(115, 2.84);
-        sgTee.put(120, 2.85);
-        sgTee.put(125, 2.86);
-        sgTee.put(130, 2.88);
-        sgTee.put(135, 2.90);
-        sgTee.put(140, 2.91);
-        sgTee.put(145, 2.93);
-        sgTee.put(150, 2.95);
-        sgTee.put(155, 2.97);
+        sgTee.put(0, 0.00);
+        sgTee.put(100, 2.92);
+        sgTee.put(105, 2.93);
+        sgTee.put(110, 2.95);
+        sgTee.put(115, 2.95);
+        sgTee.put(120, 2.95);
+        sgTee.put(125, 2.95);
+        sgTee.put(130, 2.97);
+        sgTee.put(135, 2.97);
+        sgTee.put(140, 2.97);
+        sgTee.put(145, 2.97);
+        sgTee.put(150, 2.99);
+        sgTee.put(155, 2.99);
+        sgTee.put(160, 2.99);
+        sgTee.put(165, 3.01);
+        sgTee.put(170, 3.02);
+        sgTee.put(175, 3.04);
+        sgTee.put(180, 3.05);
+        sgTee.put(185, 3.07);
+        sgTee.put(190, 3.09);
+        sgTee.put(195, 3.11);
+        sgTee.put(200, 3.12);
+        sgTee.put(210, 3.14);
+        sgTee.put(220, 3.17);
+        sgTee.put(230, 3.21);
+        sgTee.put(240, 3.25);
+        sgTee.put(250, 3.35);
+        sgTee.put(260, 3.45);
+        sgTee.put(270, 3.55);
+        sgTee.put(280, 3.65);
+        sgTee.put(290, 3.68);
+        sgTee.put(300, 3.71);
+        sgTee.put(320, 3.79);
+        sgTee.put(340, 3.86);
+        sgTee.put(360, 3.92);
+        sgTee.put(380, 3.96);
+        sgTee.put(400, 3.99);
+        sgTee.put(420, 4.02);
+        sgTee.put(440, 4.08);
+        sgTee.put(460, 4.17);
+        sgTee.put(480, 4.28);
+        sgTee.put(500, 4.41);
+        sgTee.put(520, 4.54);
+        sgTee.put(540, 4.65);
+        sgTee.put(560, 4.74);
+        sgTee.put(580, 4.79);
+        sgTee.put(600, 4.82);
     }
 
-    public static double calculateHole(int holeLength, int strokes)
+    private static final TreeMap<Integer, Double> sgApp = new TreeMap<>();
+    static {
+        sgApp.put(0, 0.00);
+        sgApp.put(10, 2.20); sgApp.put(15, 2.30); sgApp.put(20, 2.40);
+        sgApp.put(25, 2.45); sgApp.put(30, 2.50); sgApp.put(35, 2.55);
+        sgApp.put(40, 2.60); sgApp.put(45, 2.62); sgApp.put(50, 2.65);
+        sgApp.put(55, 2.67); sgApp.put(60, 2.70); sgApp.put(65, 2.72);
+        sgApp.put(70, 2.73); sgApp.put(75, 2.74); sgApp.put(80, 2.75);
+        sgApp.put(85, 2.77); sgApp.put(90, 2.78); sgApp.put(95, 2.79);
+        sgApp.put(100, 2.80); sgApp.put(105, 2.81); sgApp.put(110, 2.83);
+        sgApp.put(115, 2.84);
+        sgApp.put(120, 2.85);
+        sgApp.put(125, 2.86);
+        sgApp.put(130, 2.88);
+        sgApp.put(135, 2.90);
+        sgApp.put(140, 2.91);
+        sgApp.put(145, 2.93);
+        sgApp.put(150, 2.95);
+        sgApp.put(155, 2.97);
+        sgApp.put(160, 2.98);
+        sgApp.put(165, 3.00);
+        sgApp.put(170, 3.03);
+        sgApp.put(175, 3.06);
+        sgApp.put(180, 3.08);
+        sgApp.put(185, 3.11);
+        sgApp.put(190, 3.13);
+        sgApp.put(195, 3.16);
+        sgApp.put(200, 3.19);
+        sgApp.put(210, 3.26);
+        sgApp.put(220, 3.32);
+        sgApp.put(230, 3.39);
+        sgApp.put(240, 3.45);
+        sgApp.put(250, 3.52);
+        sgApp.put(260, 3.58);
+        sgApp.put(270, 3.63);
+        sgApp.put(280, 3.69);
+        sgApp.put(290, 3.74);
+        sgApp.put(300, 3.78);
+        sgApp.put(320, 3.84);
+        sgApp.put(340, 3.88);
+        sgApp.put(360, 3.95);
+        sgApp.put(380, 4.03);
+        sgApp.put(400, 4.11);
+        sgApp.put(420, 4.15);
+        sgApp.put(440, 4.20);
+        sgApp.put(460, 4.29);
+        sgApp.put(480, 4.40);
+        sgApp.put(500, 4.53);
+        sgApp.put(520, 4.85);
+        sgApp.put(540, 4.97);
+        sgApp.put(560, 5.05);
+        sgApp.put(580, 5.10);
+        sgApp.put(600, 5.13);
+    }
+
+    private static final TreeMap<Integer, Double> sgAtg = new TreeMap<>();
+    static {
+//        sgAtg.put();
+    }
+
+    private static final TreeMap<Integer, Double> sgPutt = new TreeMap<>();
+    static {
+        sgPutt.put(0, 0.00);
+        sgPutt.put(3, 1.04);
+        sgPutt.put(4, 1.13);
+        sgPutt.put(5, 1.23);
+        sgPutt.put(6, 1.34);
+        sgPutt.put(7, 1.42);
+        sgPutt.put(8, 1.50);
+        sgPutt.put(9, 1.57);
+        sgPutt.put(10, 1.60);
+        sgPutt.put(11, 1.67);
+        sgPutt.put(12, 1.70);
+        sgPutt.put(13, 1.74);
+        sgPutt.put(15, 1.77);
+        sgPutt.put(17, 1.83);
+        sgPutt.put(20, 1.88);
+        sgPutt.put(21, 1.89);
+        sgPutt.put(24, 1.92);
+        sgPutt.put(27, 1.95);
+        sgPutt.put(30, 1.98);
+        sgPutt.put(33, 2.00);
+        sgPutt.put(36, 2.03);
+        sgPutt.put(39, 2.05);
+        sgPutt.put(42, 2.08);
+        sgPutt.put(45, 2.10);
+        sgPutt.put(48, 2.12);
+        sgPutt.put(51, 2.15);
+        sgPutt.put(54, 2.17);
+        sgPutt.put(57, 2.19);
+        sgPutt.put(60, 2.22);
+        sgPutt.put(63, 2.23);
+        sgPutt.put(66, 2.24);
+        sgPutt.put(69, 2.25);
+        sgPutt.put(72, 2.27);
+        sgPutt.put(75, 2.29);
+    }
+
+    public static double calculateHole(int start, int finish, String startLoc, String finishLoc)
     {
         double gained = 0;
-        double strokeGained = getExpectedStrokes(holeLength);
-        gained = strokeGained - strokes;
+        double startSG = 0;
+        double finishSG = 0;
+
+        switch (startLoc) {
+            case "Tee":
+                startSG = nearestKey(sgTee, start);
+                break;
+            case "Fairway":
+                startSG = nearestKey(sgApp, start);
+                break;
+            case "Green":
+                startSG = nearestKey(sgPutt, start);
+                break;
+        }
+        switch (finishLoc) {
+            case "Tee":
+                finishSG = nearestKey(sgTee, finish);
+                break;
+            case "Fairway":
+                finishSG = nearestKey(sgApp, finish);
+                break;
+            case "Green":
+                finishSG = nearestKey(sgPutt, finish);
+                break;
+        }
+
+        gained = startSG - finishSG - 1;
 
         return gained;
     }
@@ -52,24 +193,22 @@ public class UtilityFunctions {
     {
         double expectedStrokes = 0;
 
-
-
         if (holeLength <= 10) expectedStrokes = 2.20;
         else if (holeLength >= 60) expectedStrokes = 2.70;
 
         return expectedStrokes;
     }
 
-    public static Long nearestKey(TreeMap<Long, Object> map, Long target) {
+    public static Double nearestKey(TreeMap<Integer, Double> map, Integer target) {
         double minDiff = Double.MAX_VALUE;
-        Long nearest = null;
-        for (long key : map.keySet()) {
+        Double gained = null;
+        for (int key : map.keySet()) {
             double diff = Math.abs((double) target - (double) key);
             if (diff < minDiff) {
-                nearest = key;
+                gained = map.get(key);
                 minDiff = diff;
             }
         }
-        return nearest;
+        return gained;
     }
 }
